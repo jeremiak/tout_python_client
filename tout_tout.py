3d264b4914cc99b00a117c4cfcf8416aeffac737
Jeremia Kimelman <jbkimelman@gmail.com>
2012-10-05 16:00:45 -0700

    native python date objects are now used

--- tout_tout.py
+++ tout_tout.py
@@ -1,3 +1,18 @@
+from dateutil.parser import parse
+
 class Tout(object):
-    def __init__(self):
-        pass
+    def __init__(self, uid=None, text=None, privacy=None, recorded_at=None, created_at=None, likes_count=0, replies_count=0, retouts_count=0, liked=False, image=None, video=None, retouted_tout=None, replied_to_tout=None, user=None, conversation=None):
+        self.uid = uid
+        self.text = text
+        self.privacy = privacy
+        self.recorded_at = parse(recorded_at)
+        self.created_at = parse(created_at)
+        self.likes_count = likes_count
+        self.replies_count = replies_count
+        self.liked = liked
+        self.image = image
+        self.video = video
+        self.retouted_tout = retouted_tout
+        self.replied_to_tout = replied_to_tout
+        self.user = user
+        self.conversation = conversation
