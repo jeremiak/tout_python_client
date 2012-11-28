016e3286546078bd058204d05d5f070f1119f0ae
Jeremia Kimelman <jbkimelman@gmail.com>
2012-11-27 17:31:03 -0800

    rename package to tout

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
-from disutils.core import setup
+#!/usr/bin/python
 
-setup(name="tout",
-        version="0.5",
-        description="Python client and wrapper for the Tout API",
-        author="Jeremia Kimelman",
-        author_email="jeremia@tout.com",
-        url="http://developer.tout.com",
+from distutils.core import setup
+
+setup(name='tout',
+        version='0.5',
+        description='Python client and wrapper for the Tout API',
+        author='Jeremia Kimelman',
+        author_email='jeremia@tout.com',
+        url='http://developer.tout.com',
+        #package_dir = {'': 'client'},
+        packages = ['tout'],
         )
