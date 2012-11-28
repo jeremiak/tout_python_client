48520f0b326ccc7f74d0c2b0e29cccba42bc663f
Jeremia Kimelman <jbkimelman@gmail.com>
2012-11-27 17:19:40 -0800

    initial setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from disutils.core import setup
+
+setup(name="tout",
+        version="0.5",
+        description="Python client and wrapper for the Tout API",
+        author="Jeremia Kimelman",
+        author_email="jeremia@tout.com",
+        url="http://developer.tout.com",
+        )
