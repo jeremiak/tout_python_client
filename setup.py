a432ad225a73553c28a65d2b601d3bc9bd4860d8
Jeremia Kimelman <jbkimelman@gmail.com>
2012-12-02 03:38:18 -0800

    required for install and license

--- setup.py
+++ setup.py
@@ -3,11 +3,13 @@
 from distutils.core import setup
 
 setup(name='tout',
-        version='0.5',
+        version='0.1',
         description='Python client and wrapper for the Tout API',
         author='Jeremia Kimelman',
         author_email='jeremia@tout.com',
         url='http://developer.tout.com',
         #package_dir = {'': 'client'},
         packages = ['tout'],
+        install_requires=['requests', 'simplejson'],
+        license='MIT',
         )
